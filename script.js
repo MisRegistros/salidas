575541b0f06d8652af433fb80f0570ecc83f6de8
MisRegistros <pedrin1993152@gmail.com>
2024-11-27 18:22:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-function toggleStatus(element, type) {
-    const sibling = type === 'tick' ? element.nextElementSibling : element.previousElementSibling;
+// script.js
+function guardarDatos() {
+    for (let i = 1; i <= 30; i++) {
+        let salida = document.getElementById(`salida${i}`).value;
+        let donde = document.getElementById(`donde${i}`).value;
+        let acompanado = document.getElementById(`acompanado${i}`).value;
+        let comentarios = document.getElementById(`comentarios${i}`).value;
+        let valoracion = document.getElementById(`valoracion${i}`).value;
 
-    if (!element.classList.contains('active')) {
-        element.classList.add('active');
-        sibling.style.display = 'none';
-    } else {
-        element.classList.remove('active');
-        sibling.style.display = 'inline';
+        console.log(`Día ${i}:`);
+        console.log(`Salida: ${salida}`);
+        console.log(`Dónde: ${donde}`);
+        console.log(`Acompañado: ${acompanado}`);
+        console.log(`Comentarios: ${comentarios}`);
+        console.log(`Valoración: ${valoracion}`);
     }
 }
+
